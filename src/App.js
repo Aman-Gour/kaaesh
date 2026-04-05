61dea0e2badb8d46ebc5a46120e3279f443f6f16
Aman Gour <gouraman208@gmail.com>
2026-04-05 15:18:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate,
+  
 } from "react-router-dom";
 
 import Footer from "./components/Footer/Footer";
--- src/components/Aboutus/About.jsx
+++ src/components/Aboutus/About.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import "./about.css"
 import about from "./aboutus.jpeg"
-import Pizza from "../pizza.png"
+
 import Burger from "./burger.jpeg"
-import cofee from "./cofee.jpeg"
-import flour from "./flour.jpeg"
+
 import Navbar from "../navbar/Navbar"
 export default function About() {
   return (
@@ -16,25 +15,25 @@ export default function About() {
       <div id='about_discover_our_heading'>
         <h1>Discover Our</h1>
         <h1> Restaurant Story</h1>
-        <img src={about}></img>
+        <img src={about} alt='s'></img>
         <p>This is Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe dolore at aspernatur eveniet temporibus placeat voluptatum quaerat accusamus possimus cupiditate, quidem impedit sed libero id perspiciatis esse earum repellat quam. Dolore modi temporibus quae possimus accusantium, cum corrupti sed deserunt iusto at sapiente nihil sint iste similique soluta dolor! Quod.</p>
 
       </div>
 
       <div id='about_us_img_corn'>
-        <img src="https://wallpapers.com/images/featured/food-4k-1pf6px6ryqfjtnyr.jpg" />
+        <img src="https://wallpapers.com/images/featured/food-4k-1pf6px6ryqfjtnyr.jpg"  alt='s' />
       </div>
 
 
      
       <div id='about_discover_our_heading'>
         <h1> Latest Publications</h1>
-        <img src={about}></img>
+        <img src={about}  alt='s'></img>
 
       </div>
       <div id='flex_publications_card_aboutus'>
         <div id='Latest_card_aboutus'>
-          <img src={Burger} />
+          <img src={Burger}  alt='s' />
           <p>September.15.2021</p>
           <h4>Energy Drink Which You Can Make At Home.</h4>
           <h5>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur ipsa explicabo atque reprehenderit beatae! Accusantium soluta consequuntur blanditiis amet ad.</h5>
@@ -42,7 +41,7 @@ export default function About() {
         </div>
 
         <div id='Latest_card_aboutus'>
-          <img src="https://c4.wallpaperflare.com/wallpaper/17/1007/659/coffee-drinking-glass-coffee-beans-food-hd-wallpaper-preview.jpg" />
+          <img src="https://c4.wallpaperflare.com/wallpaper/17/1007/659/coffee-drinking-glass-coffee-beans-food-hd-wallpaper-preview.jpg"  alt='s'/>
           <p>September.15.2021</p>
           <h4>Energy Drink Which You Can Make At Home.</h4>
           <h5>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur ipsa explicabo atque reprehenderit beatae! Accusantium soluta consequuntur blanditiis amet ad.</h5>
@@ -50,7 +49,7 @@ export default function About() {
         </div>
 
         <div id='Latest_card_aboutus'>
-          <img src="https://img.freepik.com/free-photo/top-view-roasted-vegetables-roasted-vegetables-plate_140725-72438.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1720396800&semt=ais_user" />
+          <img src="https://img.freepik.com/free-photo/top-view-roasted-vegetables-roasted-vegetables-plate_140725-72438.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1720396800&semt=ais_user"  alt='s'/>
           <p>September.15.2021</p>
           <h4>Energy Drink Which You Can Make At Home.</h4>
           <h5>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur ipsa explicabo atque reprehenderit beatae! Accusantium soluta consequuntur blanditiis amet ad.</h5>
@@ -78,41 +77,41 @@ export default function About() {
       <div class="slider">
         <div class="slide-track">
           <div class="slide">
-            <img src="https://i.pinimg.com/736x/6a/fb/32/6afb323e30d659c27d7569276087c310.jpg" height="100" width="250" alt="" />
+            <img src="https://i.pinimg.com/736x/6a/fb/32/6afb323e30d659c27d7569276087c310.jpg" height="100" width="250" alt="s" />
           </div>
           <div class="slide">
-            <img src="https://static.vecteezy.com/system/resources/thumbnails/019/909/686/small_2x/burger-king-transparent-burger-king-free-free-png.png" height="100" width="250" alt="" />
+            <img src="https://static.vecteezy.com/system/resources/thumbnails/019/909/686/small_2x/burger-king-transparent-burger-king-free-free-png.png" height="100" width="250" alt="s" />
           </div>
           
           <div class="slide">
-            <img src="https://getdrawings.com/vectors/mcdonalds-logo-vector-35.png" height="100" width="250" alt="" />
+            <img src="https://getdrawings.com/vectors/mcdonalds-logo-vector-35.png" height="100" width="250" alt="s" />
           </div>
 
           <div class="slide">
-            <img src=" https://pngimg.com/d/kfc_PNG51.png" height="100" width="250" alt="" />
+            <img src=" https://pngimg.com/d/kfc_PNG51.png" height="100" width="250" alt="s" />
           </div>
 
           <div class="slide">
-            <img src="https://businessyield.com/wp-content/uploads/2022/10/pizza-hut.jpg" height="100" width="250" alt="" />
+            <img src="https://businessyield.com/wp-content/uploads/2022/10/pizza-hut.jpg" height="100" width="250" alt="s" />
           </div>
       
           <div class="slide">
-            <img src="https://png.pngitem.com/pimgs/s/195-1956278_dominos-pizza-logo-png-transparent-png.png" height="100" width="250" alt="" />
+            <img src="https://png.pngitem.com/pimgs/s/195-1956278_dominos-pizza-logo-png-transparent-png.png" height="100" width="250" alt="s" />
           </div>
           <div class="slide">
-            <img src="https://logowik.com/content/uploads/images/866_wendys.jpg" height="100" width="250" alt="" />
+            <img src="https://logowik.com/content/uploads/images/866_wendys.jpg" height="100" width="250" alt="s" />
           </div>
           <div class="slide">
-            <img src="https://i.pinimg.com/736x/4a/c9/6c/4ac96cf93e07bb096431538005c1b077.jpg" height="100" width="250" alt="" />
+            <img src="https://i.pinimg.com/736x/4a/c9/6c/4ac96cf93e07bb096431538005c1b077.jpg" height="100" width="250" alt="s" />
           </div>
           <div class="slide">
-            <img src="https://www.citypng.com/public/uploads/preview/hd-subway-fast-food-restaurant-logo-png-11653305767bkf5zn3sjz.png" height="100" width="250" alt="" />
+            <img src="https://www.citypng.com/public/uploads/preview/hd-subway-fast-food-restaurant-logo-png-11653305767bkf5zn3sjz.png" height="100" width="250" alt="s" />
           </div>
           <div class="slide">
-            <img src="https://img.freepik.com/free-vector/vegetarian-food-menu_24877-50862.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1721001600&semt=ais_user" height="100" width="250" alt="" />
+            <img src="https://img.freepik.com/free-vector/vegetarian-food-menu_24877-50862.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1721001600&semt=ais_user" height="100" width="250" alt="s" />
           </div>
           <div class="slide">
-            <img src="https://mostaql.hsoubcdn.com/uploads/portfolios/2319176/642e93564bfcc/restaurant-logo-design-template-08a68b9418adeb6c8599892284a22ef1screen.jpg" height="100" width="250" alt="" />
+            <img src="https://mostaql.hsoubcdn.com/uploads/portfolios/2319176/642e93564bfcc/restaurant-logo-design-template-08a68b9418adeb6c8599892284a22ef1screen.jpg" height="100" width="250" alt="s" />
           </div>
 
         </div>
